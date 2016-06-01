1be116a07bd796a460c3e47869abd4c1e82e49b3
Jimages <jimages123@gmail.com>
2016-06-02 00:35:38 +0800

    Initialize log module.

--- src/log.c
+++ src/log.c
@@ -14,7 +14,15 @@ static FILE *log_file;
 
 int LogInit(void) {
     extern FILE *log_file;
+    const char * config_value;
 
-    log_file = fopen(LOG_FILE_PATH, "a");
+    /* If run in root, save messages in log directory. Otherwise to a file specficed by configure or directly print to console. */
+        log_file = freopen(LOG_FILE_PATH, "a", stdout);
+        log_file = freopen(LOG_FILE_PATH, "a", stderr);
+    if(log_file == NULL && (config_value = GetConfig("LOG_FILE_PATH")) != NULL) {
+        log_file = freopen(config_value, "a", stderr);
+        log_file = freopen(config_value, "a", stdout);
+    } 
+    /* there is no need to check error, because log will be sent to consule.*/
     return 0;
 }
